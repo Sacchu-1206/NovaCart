d5b02b476b386adb8ab1419df0a807da09c22013
Sacchu-1206 <sachinyadav692003@gmail.com>
2026-06-27 18:06:40 +0530

    Create products page

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,6 +1,53 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import ProductCard from "../../components/product/ProductCard";
+
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getProducts = async () => {
+    try {
+      const response = await api.get("/products");
+      setProducts(response.data.products);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  if (loading) {
+    return (
+      <h2 className="text-center text-3xl py-20">
+        Loading Products...
+      </h2>
+    );
+  }
+
   return (
-    <h1>Products Page</h1>
+    <section className="max-w-7xl mx-auto px-6 py-12">
+
+      <h1 className="text-4xl font-bold mb-10">
+        All Products
+      </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        ))}
+
+      </div>
+
+    </section>
   );
 };
 
